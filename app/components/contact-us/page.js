8b07e407394ff8298a2810cd56156f800690843e
Forhad2k <fh594694@gmail.com>
2025-05-15 13:35:55 +0600

    Contact connect to db

--- app/components/contact-us/page.js
+++ app/components/contact-us/page.js
@@ -25,7 +25,7 @@ const ContactUs = () => {
     setIsSubmitting(true);
 
     try {
-      const response = await fetch('https://editor-cloud.onrender.com', {
+      const response = await fetch('https://editor-cloud-main.onrender.com', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
