3a1e423c84d9631093611997e06573d6727d366a
Forhad2k <fh594694@gmail.com>
2025-05-15 10:48:02 +0600

    contact

--- app/components/contact-us/page.js
+++ app/components/contact-us/page.js
@@ -25,7 +25,7 @@ const ContactUs = () => {
     setIsSubmitting(true);
 
     try {
-      const response = await fetch('http://localhost:5000/clientdata', {
+      const response = await fetch('https://editor-cloud.onrender.com', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
